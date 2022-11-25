4b200d66fc5d87e31a356335d2088f0169756697
nataliaschmidt <nataliak.schmidt@gmail.com>
2022-11-25 19:53:31 -0300

    Feito até o 4º exercício

--- fundamentos/secao-04-javascript-introducao-a-javascript/dia-02-array-e-for/script1.js
+++ fundamentos/secao-04-javascript-introducao-a-javascript/dia-02-array-e-for/script1.js
@@ -1,8 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Percorra o array imprimindo todos os valores nele contidos com a função console.log();
-// for (let i = 0; i < numbers.length; i += 1){
-//     console.log(numbers[i]);
+// for (value of numbers){
+//     console.log(value);
 // }
 
 // Some todos os valores contidos no array e imprima o resultado;
@@ -13,11 +13,26 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 //Calcule e imprima a média aritmética dos valores contidos no array;
-let sum = 0;
-let average = 0;
+// faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 
-for (let i = 0; i < numbers.length; i += 1){
-sum += numbers[i];
+// let sum = 0;
+// let average = 0;
+
+// for (let i = 0; i < numbers.length; i += 1){
+// sum += numbers[i];
+// }
+// average = sum/numbers.length;
+// if (average > 20){
+//     console.log('valor maior que 20');
+// } else {
+//     console.log('valor menor do que 20');
+// }
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+let higherNumber = numbers[0];
+for (value of numbers){
+if (value > higherNumber){
+    higherNumber = numbers[value]
+    console.log(higherNumber);
 }
-average = sum/numbers.length;
-console.log(average);
+}
