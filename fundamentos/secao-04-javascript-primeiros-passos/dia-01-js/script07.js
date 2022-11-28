cf84e37d8fa09627916de981f1fdf67b2772e982
nataliaschmidt <nataliak.schmidt@gmail.com>
2022-11-28 18:53:15 -0300

    WIP: exercício salário

---
+++ fundamentos/secao-04-javascript-primeiros-passos/dia-01-js/script07.js
@@ -0,0 +1,41 @@
+//Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+//Bonus: use somente um if.
+
+// const number01 = 5;
+// const number02 = 3;
+// const number03 = 15;
+
+// if (number01 % 2 === 0 || number02 % 2 === 0 || number03 % 2 === 0){
+//     return console.log('true');
+// } else {
+//     return console.log('false');
+// }
+
+
+
+// Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+//Bonus: use somente um if.
+
+const number01 = 5;
+const number02 = 4;
+const number03 = 15;
+
+if (number01 % 2 != 0 || number02 % 2 != 0 || number03 % 2 != 0){
+    return console.log('true');
+} else {
+    return console.log('false');
+}
+
+
+
+//Solução do gabarito
+// const a = 1;
+// const b = 3;
+// const c = 5;
+
+// let isEven = false;
+
+// if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0)) {
+//   isEven = true;
+// };
+// console.log(isEven);
