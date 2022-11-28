d1fca6d56b79f8ab0240ad9ef1b1676706d65bec
nataliaschmidt <nataliak.schmidt@gmail.com>
2022-11-28 20:14:39 -0300

    Adicionando o enunciado

--- fundamentos/secao-04-javascript-introducao-a-javascript/dia-02-array-e-for/script2.js
+++ fundamentos/secao-04-javascript-introducao-a-javascript/dia-02-array-e-for/script2.js
@@ -7,6 +7,7 @@ for (let i = 1; i <= 25; i+= 1){
 
 console.log(array);
 
+// Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
 for (let i = 0; i < array.length; i += 1){
     console.log(`${array[i]} / 2 =` + array[i] / 2);
 }
