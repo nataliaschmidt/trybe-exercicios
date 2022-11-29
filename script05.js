56d57d304a2fa7907a889c61947f1a3b376fa9b5
nataliaschmidt <nataliak.schmidt@gmail.com>
2022-11-29 15:42:05 -0300

    Dia 03 de JS

---
+++ script05.js
@@ -0,0 +1,8 @@
+let array = [2, 5,  9, 10, 20, 15, 25, 33, 50, 62];
+const number = 9;
+
+for (let i = 0; i < array.length; i += 1){
+    if (number == array[i]){
+        console.log(array.findIndex[i])
+    }
+}
