4ab770d4ad86aab26eb775ade3f2471805e7e885
nataliaschmidt <nataliak.schmidt@gmail.com>
2022-12-07 20:44:49 -0300

    Alterado o texto do título

--- fundamentos/secao-05-doom-eventos-web_storage/dia-01-doom-e-seletores/script.js
+++ fundamentos/secao-05-doom-eventos-web_storage/dia-01-doom-e-seletores/script.js
@@ -2,6 +2,7 @@
 //document.getElementsByClassName();
 //document.getElementsByTagName();
 
+//Crie e execute uma função que mude o texto na tag <p>-----</p>, para uma descrição de como você se vê daqui a 2 anos;
 function newText(){
   let future =  document.getElementsByTagName('p')[1];
     future.innerHTML = `Daqui 02 anos me vejo trabalhando como desenvolvedora e decolando na minha carreira!`;   
@@ -9,9 +10,25 @@ function newText(){
 
 newText()
 
+//Crie e execute uma função que mude a cor do quadrado amarelo para rgb(76,164,109);
 function newColor (){
     let newColor = document.getElementsByClassName('main-content')[0]
     newColor.style.background = 'rgb(76,164,109)';
 }
 
 newColor ();
+//Crie e execute uma função que mude a cor do quadrado vermelho para branco;
+function midleColor (){
+  let midleColor = document.getElementsByClassName('center-content')[0]
+  midleColor.style.background = 'white'
+}
+
+midleColor()
+
+//Crie e execute uma função que corrija o texto da tag <h1>;
+const correctText = () => {
+  let title = document.getElementsByTagName('h1')[0]
+  title.innerHTML = `Desafio - JavaScript`
+}
+
+correctText()
