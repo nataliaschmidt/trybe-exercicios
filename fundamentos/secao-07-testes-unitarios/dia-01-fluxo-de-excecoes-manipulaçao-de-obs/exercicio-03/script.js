aa0f5bf92f076ecb65a3b66be11479a1f6c33805
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-01-22 11:16:35 -0300

    exercicio bonus resolvido

--- fundamentos/secao-07-testes-unitarios/dia-01-fluxo-de-excecoes-manipulaçao-de-obs/exercicio-03/script.js
+++ fundamentos/secao-07-testes-unitarios/dia-01-fluxo-de-excecoes-manipulaçao-de-obs/exercicio-03/script.js
@@ -23,7 +23,7 @@ const lesson3 = {
 const addTurn = (obj, key, value) => {
   obj[key] = value;
   return obj
-} 
+}
 
 console.log(addTurn(lesson2, 'turno', 'noite'));
 console.log('---------------------------------------------------------------------------------------------');
@@ -50,25 +50,25 @@ console.log('-------------------------------------------------------------------
 
 // Crie um objeto de nome allLessons, que deve agrupar todas as aulas
 
-const allLessons = Object.assign({}, {lesson1, lesson2, lesson3})
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 })
 console.log(allLessons);
 console.log('---------------------------------------------------------------------------------------------');
 
 // Usando o objeto criado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
 
 const totalnumberStudants = (obj) => {
-let total = 0; // iniciar a contagem de alunos com 0
-const keys = Object.keys(obj); // vamos pegar as chaves do objeto [ lesson1, lesson2, lesson3 ] -> retorna um array
-// console.log(keys);
-for (index in keys) { //for in para iterar sobre o array de chaves 
- // console.log(keys[index]);// -> lesson1, lesson2, lesson3
- // console.log(obj[keys[index]]); -> vou conseguir acesso a cada um dos objetos
+  let total = 0; // iniciar a contagem de alunos com 0
+  const keys = Object.keys(obj); // vamos pegar as chaves do objeto [ lesson1, lesson2, lesson3 ] -> retorna um array
+  // console.log(keys);
+  for (index in keys) { //for in para iterar sobre o array de chaves 
+    // console.log(keys[index]);// -> lesson1, lesson2, lesson3
+    // console.log(obj[keys[index]]); -> vou conseguir acesso a cada um dos objetos
 
-  // console.log(obj[keys[index]]['numeroEstudantes']) // 20, 20, 10.. acessando o número de estudantes keys index do obj parâmetro 
+    // console.log(obj[keys[index]]['numeroEstudantes']) // 20, 20, 10.. acessando o número de estudantes keys index do obj parâmetro 
 
-  total += obj[keys[index]]['numeroEstudantes'];
-}
-return total;
+    total += obj[keys[index]]['numeroEstudantes'];
+  }
+  return total;
 }
 console.log(`Total de alunos é: ${totalnumberStudants(allLessons)}`);
 console.log('---------------------------------------------------------------------------------------------');
@@ -77,7 +77,7 @@ console.log('-------------------------------------------------------------------
 
 const getValue = (obj, number) => {
   // Object.values vai retornar um array com os valores do objetos, posso utilizar o [index] para acessar o valor que eu quero daquele array
- return Object.values(obj)[number]
+  return Object.values(obj)[number]
 }
 
 console.log(getValue(lesson1, 2));
@@ -91,12 +91,61 @@ const verifyPair = (obj, chave, valor) => {
   // console.log(entries);
   let isEqual = false;
 
-  for (let index in entries){
-console.log(entries[index]);
-if (entries[index][0] === chave && entries[index][1] === valor) isEqual = true
-// se matéria for igual a chave passada e Matemática for igual ao valor passado retorna true
+  for (let index in entries) {
+    console.log(entries[index]);
+    if (entries[index][0] === chave && entries[index][1] === valor) isEqual = true
+    // se matéria for igual a chave passada e Matemática for igual ao valor passado retorna true
   }
   return isEqual;
 
 }
 console.log(verifyPair(lesson1, 'turno', 'tarde'));
+console.log('---------------------------------------------------------------------------------------------');
+
+// ------------------------------------------------ // --------------------------------------------------------- //
+
+//  EXERCÍCIO BÔNUS
+
+// Utilizando o objeto (allLesson), crie uma função para contar quantos estudantes assistiram às aulas de Matemática;
+
+const studantsMath = (obj) => {
+  let total = 0;
+  const keys = Object.keys(obj); // retorna o array [lesson1, lesson2, lesson3]
+
+  for (index in keys) {
+    // console.log(obj[keys[index]].materia);
+    if (obj[keys[index]].materia === 'Matemática') {
+      total += obj[keys[index]].numeroEstudantes;
+    }
+  }
+  return total;
+}
+
+console.log(studantsMath(allLessons));
+console.log('---------------------------------------------------------------------------------------------');
+
+// Utilizando o objeto (allLesson), crie uma função que deva retornar um objeto que represente o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
+
+const getInfo = (obj, name) => {
+  const allLessons = [];
+  let allStudent = 0;
+  const values = Object.values(obj);
+   // console.log(values);
+   for (index in values) {
+    if (values[index].professor === name) {
+      // console.log(`essa é a matéria ${array[index].materia}`);
+      // console.log(`esses são os estudantes ${array[index].numeroEstudantes}`);
+      allLessons.push(values[index].materia)
+      allStudent += values[index].numeroEstudantes;
+    }
+  }
+  return { aulas: allLessons, estudantes: allStudent };
+}
+
+const createReport = (allLessons, name) => {
+  const report = {};
+  report.professor = name;
+  Object.assign(report, getInfo(allLessons, name));
+  return report;
+}
+console.log(createReport(allLessons, 'Maria Clara'));
