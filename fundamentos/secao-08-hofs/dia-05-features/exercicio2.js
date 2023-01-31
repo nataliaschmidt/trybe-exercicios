c1a4c40ba8af97265959168407177c831b542932
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-01-31 12:00:38 -0300

    exercicio 2 e 3 realizados

---
+++ fundamentos/secao-08-hofs/dia-05-features/exercicio2.js
@@ -0,0 +1,9 @@
+// Suponha que você esteja trabalhando em projeto de um site de carros, onde cada carro é representado dentro de um array. Então sua liderança pede que seja mudado o formato de array para objeto. Para isso, crie uma função chamada toObject que, dada uma lista, retorna um objeto representando o carro:
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([carro, marca, ano]) => ({carro, marca, ano,})
+
+console.log(toObject(palio));
