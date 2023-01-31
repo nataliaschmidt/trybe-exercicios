493d89dd14bc7194e51475931bd250e789a78673
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-01-31 12:10:05 -0300

    exercicios resolvidos

---
+++ fundamentos/secao-08-hofs/dia-05-features/exercicio4.js
@@ -0,0 +1,19 @@
+// Escreva uma função getLastName que receba como parâmetro um objeto contendo informações de uma pessoa. Essa função deve retornar a propriedade lastName para o objeto passado, porém, caso o objeto não tenha essa propriedade, a função deve retornar a mensagem lastName não preenchido.
+
+const student1 = {
+  name: `Claudia`,
+  lastName: `Farias`,
+  age: 23,
+}
+
+const student2 = {
+  name: `Vitor`,
+  age: 20,
+}
+
+const getLastName = ({lastName = 'lastName não preenchido'}) => {
+return lastName
+}
+
+console.log(getLastName(student1));
+console.log(getLastName(student2));
