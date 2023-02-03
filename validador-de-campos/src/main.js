07126a460d34a8330c9d2741dbef95a984e9b713
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-02-02 22:18:01 -0300

    Exercicio completo com CSS formatado

--- validador-de-campos/src/main.js
+++ validador-de-campos/src/main.js
@@ -14,15 +14,15 @@ getBtn.addEventListener('click', (event) => {
   // serem validados. Por exemplo, a chave CPF valida se
   // o campoDeTexto.value é um CPF.
   const options = {
+    email: validator.isEmail(getInput.value),
     cpf: validator.isTaxID(getInput.value, 'pt-BR'),
     estaEmBranco: validator.isEmpty(getInput.value),
-    email: validator.isEmail(getInput.value),
     isDate: validator.isDate(getInput.value),
     url: validator.isURL(getInput.value),
     isDivisible: validator.isDivisibleBy(getInput.value, 2),
     isLowerCase: validator.isLowercase(getInput.value),
   };
-
+ 
   // O objeto 'campos' possui as chaves com o mesmo nome
   // das opções do seletor em nossa página. Assim, podemos
   // selecionar a chave de acordo com o selecionado no HTML
