dbdf788e28b1a236e8ffde96950fba16ab3cec0f
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-06-13 20:49:15 -0300

    Exercicios bonus resolvidos

---
+++ back-end/secao-04-Node.js/dia-01/exercicio-04-bonus/index.js
@@ -0,0 +1,45 @@
+function doMath(a, b, c) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+      return reject(new Error('Informe apenas números')); 
+    }
+
+    const result = (a + b) * c;
+
+    if (result < 50) {
+      return reject(new Error('Valor muito baixo'));
+    }
+
+    resolve(result);
+  });
+}
+
+async function firstResolve() {
+  try {
+  const resolve = await doMath(10, 10, 10);
+  console.log(resolve);
+  } catch (error) {
+  console.log(error.message);
+  }
+}
+firstResolve();
+
+async function secondResolve() {
+  try {
+  const resolve = await doMath(1, 1, 'a');
+  console.log(resolve);
+  } catch (error) {
+  console.log(error.message);
+  }
+}
+secondResolve();
+
+async function thirdResolve() {
+  try {
+  const resolve = await doMath(1, 1, 1);
+  console.log(resolve);
+  } catch (error) {
+  console.log(error.message);
+  }
+}
+thirdResolve();
