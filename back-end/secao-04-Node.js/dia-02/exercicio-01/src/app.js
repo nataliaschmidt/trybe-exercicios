0c155119096f54b6d2326beabe855d56ba908b7c
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-06-14 16:12:39 -0300

    Exercicio Bônus resolvido

--- back-end/secao-04-Node.js/dia-02/exercicio-01/src/app.js
+++ back-end/secao-04-Node.js/dia-02/exercicio-01/src/app.js
@@ -1,4 +1,3 @@
-const { log } = require('console');
 const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
@@ -12,23 +11,41 @@ app.use(express.json());
 const readFile = async () => {
   try {
     const data = await fs.readFile(moviesPath);
-    return JSON.parse(data)
+    return JSON.parse(data);
   } catch (error) {
-    console.error(`Arquivo não pode ser lido: ${error}`)
+    console.error(`Arquivo não pode ser lido: ${error}`);
   }
+};
+
+// Endpoint que possa listar todos os filmes do JSON
+// A rota deve receber a informação por query e a chave deve-se chamar q. A chave vai trazer consigo valor de ‘gente’ por exemplo, e o filtro deve trazer apenas os filmes com esse termo, se não encontrar, traga um array vazio.
+app.get('/movies/search', async (req, res) => {
+try {
+  const { q } = req.query;
+  // http://localhost:3001/movies/search?q=gente
+  // console.log(q);
+  const movies = await readFile();
+  if (q) {
+    const regex = new RegExp(q, 'i');
+    const filteredMovies = movies.filter((element) => regex.test(element.movie));
+    // console.log(regex);
+    return res.status(200).json(filteredMovies);
+  }
+  res.status(200).end();
+} catch (error) {
+  res.status(500).send({ message: error.message });
 }
+});
 
-//Endpoint para listar todos os filmes
+// Endpoint para listar todos os filmes
 app.get('/movies', async (req, res) => {
-
   try {
     const movies = await readFile();
-    res.status(200).json(movies)
+    res.status(200).json(movies);
   } catch (error) {
-    res.status(500).send({ message: err.message });
+    res.status(500).send({ message: error.message });
   }
-})
-
+});
 
 // Enpoint para listar um filme do JSON por id
 app.get('/movies/:id', async (req, res) => {
@@ -37,9 +54,9 @@ app.get('/movies/:id', async (req, res) => {
     const movie = movies.find(({ id }) => id === Number(req.params.id));
     res.status(200).json(movie);
   } catch (error) {
-    res.status(500).send({ message: err.message });
+    res.status(500).send({ message: error.message });
   }
-})
+});
 
 // Endpoint para cadastrar um novo filme no arquivo JSON
 app.post('/movies', async (req, res) => {
@@ -55,9 +72,9 @@ app.post('/movies', async (req, res) => {
     await fs.writeFile(moviesPath, allMovies);
     res.status(201).json(newMovie);
   } catch (error) {
-    res.status(500).send({ message: err.message });
+    res.status(500).send({ message: error.message });
   }
-})
+});
 
 // Endpoint para editar as informações de um filme do JSON
 app.put('/movies/:id', async (req, res) => {
@@ -65,17 +82,17 @@ app.put('/movies/:id', async (req, res) => {
     const { movie, price } = req.body;
     const { id } = req.params;
     const movies = await readFile();
-    const index = movies.findIndex(e => e.id === Number(id));
+    const index = movies.findIndex((e) => e.id === Number(id));
     movies[index] = {
       id: Number(id),
       movie,
-      price
+      price,
     };
     const updatedMovies = JSON.stringify(movies, null, 2);
     await fs.writeFile(moviesPath, updatedMovies);
     res.status(200).json(movies[index]);
   } catch (error) {
-    res.status(500).send({ message: err.message });
+    res.status(500).send({ message: error.message });
   }
 });
 
@@ -89,9 +106,8 @@ try {
     await fs.writeFile(moviesPath, updatedMovies);
     res.status(204).end();
 } catch (error) {
-  res.status(500).send({ message: err.message });
+  res.status(500).send({ message: error.message });
 }
-})
-
+});
 
 module.exports = app;
